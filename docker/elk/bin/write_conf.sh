e1548423f308796b138c94ee960f94ba594523ef
Tristan Pettinotti <tristan@zol.fr>
2017-06-27 12:00:37 +0200

    leave /queries directory in build parameters, and change its value to /queries for simplicity

--- docker/elk/bin/write_conf.sh
+++ docker/elk/bin/write_conf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /opt/logstash/config/custom/
 
-for file in /root/queries/*.sql; do
+for file in /queries/*.sql; do
 	filename=$(basename "$file")
     /root/app/template.sh $CONNEXION_STRING $CONNEXION_USER $CONNEXION_PWD $file ${filename%.*} > /opt/logstash/config/custom/${filename%.*}.conf
 	echo "[added] ${filename} "
